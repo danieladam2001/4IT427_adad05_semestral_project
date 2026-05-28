38c8af4b09efd73fc64174d291a54f7125c5834e
Daniel Adam <danadam2001@seznam.cz>
2026-05-28 16:06:48 +0200

    feat: Inicializace nového repa

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+  <main>
+    Semestral Project
+  </main>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts (vznikne při instalaci)
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test-setup.ts',
+  },
+  resolve: {
+    alias: { '@': '/src' },
+  },
+});
