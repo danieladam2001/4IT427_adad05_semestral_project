7150f73b65dd10e4a21706ec45c0534f0d853197
Daniel Adam <danadam2001@seznam.cz>
2026-06-01 13:34:47 +0200

    feat: Fetchování pipeline ze souboru

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,25 @@ import App from './App.tsx'
 import { PipelineProvider } from './context/PipelineContext.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, 
+      retry: 2,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <PipelineProvider>
-        <App />
-      </PipelineProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <PipelineProvider>
+          <App />
+        </PipelineProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
